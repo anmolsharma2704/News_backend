82d2fb62d61232b0382c17d39251846dd6867c9f
Anmol <2704anmolsharma@gmail.com>
2024-06-19 18:06:09 +0530

    modified files

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,3 +31,12 @@ exports.activateUser = async (req, res) => {
         res.status(500).send('Server error');
     }
 };
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { suspendUser, activateUser } = require('../controllers/userController');
+const { suspendUser, activateUser,getAllUsers } = require('../controllers/userController');
 const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
 
 router.patch('/suspend/:id', authMiddleware, adminMiddleware, suspendUser);
 router.patch('/activate/:id', authMiddleware, adminMiddleware, activateUser);
+router.get('/all', authMiddleware, adminMiddleware, getAllUsers);
 
 module.exports = router;
